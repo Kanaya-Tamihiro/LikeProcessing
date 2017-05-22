1d44a2f01388f6a0615cc6753fbe302a2b53b98b
tamichang <hogehogegehegehe@gmail.com>
2017-05-23 02:30:38 +0900

    Add PLineSimple

--- Assets/LikeProcessing/Examples/Primitives/Primitives.cs
+++ Assets/LikeProcessing/Examples/Primitives/Primitives.cs
@@ -8,12 +8,18 @@ namespace LikeProcessig.Examples {
 	public class Primitives : PSketch {
 
 		PGeodesicDomeImproved pgeodesicDome;
+		PLineSimple plineSimple;
 
 		void Start () {
 			lightObj.GetComponent<Light> ().intensity = 0.8f;
+
 			pgeodesicDome = new PGeodesicDomeImproved(3);
-			pgeodesicDome.gameObject.transform.position += new Vector3 (0, 1, 0);
+			pgeodesicDome.gameObject.transform.position += new Vector3 (-2, 1, 0);
 			pgeodesicDome.gameObject.GetComponent<MeshRenderer> ().material = new Material (Shader.Find("Standard"));
+
+			plineSimple = new PLineSimple(new Vector3(0, 0, 0), new Vector3(1, 2, 0));
+			plineSimple.gameObject.GetComponent<MeshRenderer> ().material = new Material (Shader.Find("Standard"));
+
 		}
 
 		void Update () {
---
+++ Assets/LikeProcessing/Scripts/PLineSimple.cs
@@ -0,0 +1,95 @@
+﻿using UnityEngine;
+
+namespace LikeProcessing
+{
+	public class PLineSimple
+	{
+		float weight;
+		int detail;
+		public GameObject gameObject;
+		Vector3 from, to;
+		Vector3[] vertices;
+		int[] triangles;
+
+		public PLineSimple (Vector3 _from, Vector3 _to, float _weight = 0.1f, int _detail = 6)
+		{
+			gameObject = new GameObject();
+			gameObject.name = "PLineSimple";
+			gameObject.AddComponent<MeshFilter> ();
+			gameObject.AddComponent<MeshRenderer> ().material = new Material(Shader.Find("LikeProcessing/VertexColor"));
+			weight = _weight;
+			detail = _detail;
+			from = _from;
+			to = _to;
+			SetMesh ();
+		}
+
+		public void SetMesh() {
+			Vector3 center = (from + to) / 2;
+			gameObject.transform.position = center;
+			Vector3 _from = from - center;
+			Vector3 _to = to - center;
+			Vector3 fromTo = _to - _from;
+			fromTo.Normalize ();
+			Vector3 randv = PSketch.randomVector ();
+			while (Vector3.Angle(fromTo, randv) < 5.0f) {
+				randv = PSketch.randomVector();
+			}
+			Vector3 v1 = Vector3.Cross (fromTo, randv);
+			v1.Normalize ();
+			Vector3 v2 = Vector3.Cross (fromTo, v1);
+			v2.Normalize ();
+			v1 = v1 * weight;
+			v2 = v2 * weight * -1;
+			Vector3[] fromPoints = new Vector3[detail];
+			Vector3[] toPoints = new Vector3[detail];
+			float theta = 2f * Mathf.PI / (float) detail;
+			for(int i=0; i<detail; i++) {
+				Vector3 point = v1 * Mathf.Cos (theta * i) + v2 * Mathf.Sin (theta * i);
+				fromPoints [i] = _from + point;
+				toPoints [i] = _to + point;
+			}
+			triangles = new int[detail * 2 * 3];
+			int index = 0;
+			for (int i = 0; i < detail; i++) {
+				if (i == detail - 1) {
+					triangles [index] = detail + i;
+					triangles [index + 1] = 0;
+					triangles [index + 2] = i;
+					triangles [index + 3] = detail;
+					triangles [index + 4] = 0;
+					triangles [index + 5] = detail + i;
+				} else {
+					triangles [index] = detail + i;
+					triangles [index + 1] = i + 1;
+					triangles [index + 2] = i;
+					triangles [index + 3] = detail + i + 1;
+					triangles [index + 4] = i + 1;
+					triangles [index + 5] = detail + i;
+				}
+				index += 6;
+ 			}
+			vertices = new Vector3[detail * 2];
+			fromPoints.CopyTo (vertices, 0);
+			toPoints.CopyTo (vertices, detail);
+
+			Mesh mesh = gameObject.GetComponent<MeshFilter> ().mesh;
+			mesh.Clear ();
+			mesh.vertices = vertices;
+			mesh.triangles = triangles;
+			mesh.RecalculateNormals ();
+
+			Color[] colors = new Color[mesh.vertices.Length];
+			for (int i=0; i<colors.Length; i=i+1) {
+				Color color = Color.HSVToRGB (Random.value, 0.5f, 1.0f);
+				colors [i] = color;
+			}
+			mesh.colors = colors;
+		}
+
+		public void destory() {
+			Object.Destroy (this.gameObject);
+		}
+	}
+}
+
